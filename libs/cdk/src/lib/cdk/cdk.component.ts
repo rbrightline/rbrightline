c39fa5d3725bd4573e301bc8ca063c2c3129971e
Robert Brightline <robert.brightline@gmail.com>
2024-12-19 11:10:30 -0600

    created the libraries cdk, and material

---
+++ libs/cdk/src/lib/cdk/cdk.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rl-cdk',
+  imports: [CommonModule],
+  template: `<p>cdk works!</p>`,
+  styles: ``,
+})
+export class CdkComponent {}
---
+++ libs/ngrx/src/lib/ngrx/ngrx.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rl-ngrx',
+  imports: [CommonModule],
+  template: `<p>ngrx works!</p>`,
+  styles: ``,
+})
+export class NgrxComponent {}
