ae11eadbe63990b6ae0cae915e28646760a3d31d
Robert Brightline <robert.brightline@gmail.com>
2025-02-17 20:27:33 -0600

    fixed relation decorator

--- apps/rbrightline-e2e/src/rbrightline/rbrightline.spec.ts
+++ apps/rbrightline-e2e/src/rbrightline/rbrightline.spec.ts
@@ -1,10 +1,36 @@
 import axios from 'axios';
+describe('Sample Module', () => {
+  let saved: any;
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
-
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+  beforeAll(async () => {
+    const res = await axios.post(`/api/sample`, { name: 'sample' });
+    saved = res.data;
+    expect(res.status).toBe(201);
+    expect(res.data.name).toEqual('sample');
   });
-})
+
+  it('GET /api/samples', async () => {
+    const res = await axios.get(`/api/samples`);
+    expect(res.status).toBe(200);
+    expect(res.data.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('GET /api/sample/:id', async () => {
+    const res = await axios.get(`/api/sample/${saved.id}`);
+    expect(res.status).toBe(200);
+    expect(res.data.name).toEqual('sample');
+  });
+
+  it('PUT /api/sample/:id', async () => {
+    const res = await axios.put(`/api/sample/${saved.id}`, { number: 1 });
+    expect(res.status).toBe(200);
+    const found = await axios.get(`/api/sample/${saved.id}`);
+    expect(found.data.number).toEqual(1);
+  });
+
+  afterAll(async () => {
+    const res = await axios.delete(`/api/sample/${saved.id}`);
+    expect(res.status).toBe(200);
+    expect(res.data.affected).toEqual(1);
+  });
+});
